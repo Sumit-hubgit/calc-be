bd9d255e395db080c9be5b9e40d8d3b8eb4b9470
Sumit-hubgit <119445835+Sumit-hubgit@users.noreply.github.com>
2024-11-02 00:59:03 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -2,8 +2,9 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-SERVER_URL = 'localhost'
-PORT = '8900'
+SERVER_URL = '0.0.0.0'  # Bind to all interfaces
+PORT = os.getenv("PORT", "8000")  # Use the PORT environment variable or default to 8000
+
 ENV = 'dev'
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
